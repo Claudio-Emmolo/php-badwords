185478896dd890d6a0d19839e6dd9eb3e1f8c179
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-01-24 14:58:06 +0100

    Print the paragraph with the censored word

--- censure.php
+++ censure.php
@@ -3,3 +3,7 @@ $paragraph = $_GET['paragraph'];
 $censure = $_GET['censureWord'];
 
 echo ($paragraph . ', ' . strlen($paragraph));
+
+$censoredParagraph = str_ireplace($censure, '***', $paragraph);
+
+echo $censoredParagraph . strlen($censoredParagraph);
