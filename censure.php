4c0c01d5a4a2287b437a1fbeef6b866a44373be0
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-01-24 15:10:18 +0100

    Add spacing

--- censure.php
+++ censure.php
@@ -2,8 +2,8 @@
 $paragraph = $_GET['paragraph'];
 $censure = $_GET['censureWord'];
 
-echo ($paragraph . ', ' . strlen($paragraph));
+echo ($paragraph . ', ' . strlen($paragraph)); //Print the paragraph without the censored word
 
+//Print the paragraph with the censored word
 $censoredParagraph = str_ireplace($censure, '***', $paragraph);
-
-echo $censoredParagraph . strlen($censoredParagraph);
+echo (' || ' . $censoredParagraph . ', ' . strlen($censoredParagraph));
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 <body>
     <main>
-        <form action="censure.php">
+        <form action="censure.php" method="GET">
             <input type="text" name="paragraph" placeholder="Inserisci il tuo paragrafo">
             <input type="text" name="censureWord" placeholder="Inserisci parola da censurare">
             <button>Send</button>
